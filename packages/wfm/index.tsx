c57bec244216eb2585d550fcb544565e9835bad2
Nicholas Guimond <nicholas.guimond@gmail.com>
2021-02-09 15:39:09 -0400

    routing

--- packages/wfm/index.tsx
+++ packages/wfm/index.tsx
@@ -5,15 +5,17 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { QueryProvider } from './providers/queryProvider';
 import { AppThemeProvider } from './providers/appThemeProvider';
-
+import { Navigation } from './navigation';
 import styled from 'styled-components';
-const ColorButton = styled.button<{ isActive: boolean; }>`
-color: white;
-background: ${props => props.theme.colors.brand }
 
-`;
+// import styled from 'styled-components';
+// const ColorButton = styled.button<{ isActive: boolean; }>`
+// color: white;
+// background: ${props => props.theme.colors.brand }
 
-function App() {
+// `;
+
+/* function App() {
   const [ count, setCount ] = React.useState( 0 );
 
   React.useEffect( () => {
@@ -23,6 +25,41 @@ function App() {
   return <div>
     <ColorButton isActive={ false } onClick={ () => setCount( count + 1 ) }> plus 1</ColorButton>
   </div>;
+} */
+
+
+
+
+/* import PageHeader from './navigation/PageHeader';
+import SideBar from './components/SideBar';
+import PageSideBar from './navigation/PageSideBar'; */
+
+const Main = styled.main`
+  margin: 0;
+  overflow: auto;
+`;
+
+const Content = styled.section`
+  padding: 24px;
+  margin-left: 200px;
+`;
+
+export function App() {
+  return (
+    <>
+      {/* <SideBar>
+        <PageSideBar />
+      </SideBar>
+
+      <PageHeader /> */}
+
+      <Main>
+        <Content>
+          <Navigation />
+        </Content>
+      </Main>
+    </>
+  );
 }
 
 ReactDOM.render(<React.StrictMode>
---
+++ packages/wfm/navigation/index.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { Route, Switch } from 'react-router-dom';
+// import Admin from '../Scheduling/containers/Admin';
+// import Availability from '../Scheduling/containers/Agent/Availability';
+// import Messages from '../Scheduling/containers/Agent/Messages';
+// import Request from '../Scheduling/containers/Agent/Request';
+// import Schedule from '../Scheduling/containers/Agent/Schedule';
+// import Trade from '../Scheduling/containers/Agent/Trade';
+// import Forecasting from '../Forecasting';
+// import Scheduling from '../Scheduling/containers/Scheduling';
+// import NoMatch from './NoMatch';
+
+export function Navigation () {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <div>exact path</div>
+      </Route>
+      <Route exact path={ [ '/agent', '/agent/schedule' ] }>
+        <div>schedule</div>
+      </Route>
+      <Route path="/agent/availability">
+        <div>availability</div>
+      </Route>
+      <Route path="/agent/request">
+        <div>request</div>
+      </Route>
+      <Route path="/agent/trade">
+        <div>trade</div>
+      </Route>
+      <Route path="/agent/messages">
+        <div>messages</div>
+      </Route>
+
+      <Route path="/scheduling">
+        <div>sheduling</div>
+      </Route>
+      <Route path="/forecasting">
+        <div>forcasting</div>
+      </Route>
+      <Route path="/admin">
+        <div>admin</div>
+      </Route>
+      <Route path="*">
+        <div>no match</div>
+      </Route>
+    </Switch>
+  );
+}
+
+export default Navigation;
