eab2bae9dec55e76ccddc804b11c13a3e3da6e74
Nicholas Guimond <nicholas.guimond@gmail.com>
2021-05-19 14:55:16 -0300

    get token from localstorage

--- packages/wfm/src/api/index.tsx
+++ packages/wfm/src/api/index.tsx
@@ -5,9 +5,11 @@ import { forecasting } from '../redux/reducers/forecasting';
 const {
   setActiveTenant,
 } = forecasting.actions;
+const noSession = JSON.stringify({})
+const noPreference = JSON.stringify({ tenant: { tenandId: '' } });
 
-const session = localStorage.getItem('LIVEOPS-SESSION-KEY') || '{}';
-const preference = localStorage.getItem('LIVEOPS-PREFERENCE-KEY') || '{}';
+const session = localStorage.getItem('LIVEOPS-SESSION-KEY') || noSession;
+const preference = localStorage.getItem('LIVEOPS-PREFERENCE-KEY') || noPreference;
 
 const { token } = JSON.parse(session);
 const { tenant: { tenantId } } = JSON.parse(preference);
